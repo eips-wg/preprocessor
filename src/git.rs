54e95190c0cf50ff5baecbc4e9e373fadcb47f0c
Sam Wilson <sam@binarycake.ca>
2025-01-29 14:58:14 -0500

    Handle git internally

---
+++ src/git.rs
@@ -0,0 +1,287 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use std::{
+    fmt,
+    path::{Path, PathBuf},
+};
+
+use crate::progress::{Git, ProgressIteratorExt};
+use enum_map::{enum_map, Enum, EnumMap};
+use git2::{
+    build::{CheckoutBuilder, RepoBuilder, TreeUpdateBuilder},
+    BranchType, Commit, FetchOptions, FileMode, ObjectType, Repository, StatusOptions, Tree,
+    TreeWalkResult,
+};
+use log::{debug, info};
+use snafu::{ensure, Backtrace, OptionExt, Report, ResultExt, Snafu};
+use url::Url;
+
+#[derive(Debug, Snafu)]
+pub enum Error {
+    #[snafu(display("cannot convert path into URL (`{}`)", path.to_string_lossy()))]
+    PathUrl { path: PathBuf, backtrace: Backtrace },
+    #[snafu(display("unable to {what}"))]
+    Git {
+        what: &'static str,
+        source: git2::Error,
+        backtrace: Backtrace,
+    },
+    #[snafu(display("unable to determine if repository is EIPs or ERCs"))]
+    Identify { backtrace: Backtrace },
+    #[snafu(display("working tree or index has uncommitted modifications"))]
+    Dirty { backtrace: Backtrace },
+    #[snafu(display("unable to update tree ({msg})"))]
+    UpdateTree { msg: String, backtrace: Backtrace },
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Enum)]
+enum RepositoryUse {
+    Eips,
+    Ercs,
+}
+
+impl fmt::Display for RepositoryUse {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let txt = match self {
+            Self::Ercs => "ERCs",
+            Self::Eips => "EIPs",
+        };
+        write!(f, "{}", txt)
+    }
+}
+
+lazy_static::lazy_static! {
+    static ref REPO_URLS: EnumMap<RepositoryUse, &'static str> = enum_map! {
+        RepositoryUse::Eips => "https://github.com/ethereum/EIPs.git",
+        RepositoryUse::Ercs => "https://github.com/ethereum/ERCs.git",
+    };
+}
+
+impl RepositoryUse {
+    const EIP_COMMIT: &str = "0f44e2b94df4e504bb7b912f56ebd712db2ad396";
+    const ERC_COMMIT: &str = "8dd085d159cb123f545c272c0d871a5339550e79";
+
+    fn identify(repo: &Repository) -> Result<Self, Error> {
+        let eip = repo.revparse_single(Self::EIP_COMMIT);
+        let erc = repo.revparse_single(Self::ERC_COMMIT);
+
+        match (eip, erc) {
+            (Ok(_), Err(_)) => Ok(Self::Eips),
+            (Err(_), Ok(_)) => Ok(Self::Ercs),
+            (_, _) => IdentifySnafu.fail(),
+        }
+    }
+
+    fn other_repos(self) -> Vec<(Self, &'static str)> {
+        REPO_URLS.into_iter().filter(|(k, _)| *k != self).collect()
+    }
+}
+
+pub fn check_dirty(root_path: &Path) -> Result<(), Error> {
+    let repo = Repository::open(root_path).context(GitSnafu {
+        what: "open root repository",
+    })?;
+    let mut options = StatusOptions::default();
+    options.include_untracked(true);
+    let statuses = repo.statuses(Some(&mut options)).context(GitSnafu {
+        what: "get root repository status",
+    })?;
+    let mut statuses = statuses.iter().filter(|x| {
+        x.path()
+            .map(|x| !x.trim_end_matches('/').ends_with(super::BUILD_DIR))
+            .unwrap_or(false)
+    });
+    if statuses.next().is_some() {
+        DirtySnafu.fail()
+    } else {
+        Ok(())
+    }
+}
+
+fn branch_to_commit<'a, 'b>(repo: &'a Repository, rev: &'b str) -> Result<Commit<'a>, Error> {
+    repo.find_branch(rev, BranchType::Local)
+        .context(GitSnafu {
+            what: "revparse to annotated",
+        })?
+        .into_reference()
+        .peel_to_commit()
+        .context(GitSnafu {
+            what: "peel to annotated",
+        })
+}
+
+fn check_conflict(master_tree: &Tree, path: &Path) -> Result<(), Error> {
+    if path.ends_with(".gitignore") {
+        return Ok(());
+    }
+
+    let allowed = &[Path::new("content/LICENSE.md")];
+
+    if allowed.contains(&path) {
+        return Ok(());
+    }
+
+    ensure!(
+        master_tree.get_path(path).is_err(),
+        UpdateTreeSnafu {
+            msg: format!("conflicting path `{}`", path.to_string_lossy()),
+        }
+    );
+    Ok(())
+}
+
+pub fn merge_repositories(root_path: &Path, build_path: &Path) -> Result<(), Error> {
+    check_dirty(root_path)?;
+
+    let repo_path = build_path.join(super::COMBINED_DIR);
+    let url = Url::from_directory_path(root_path)
+        .ok()
+        .context(PathUrlSnafu { path: root_path })?;
+
+    if let Err(e) = std::fs::remove_dir_all(&repo_path) {
+        debug!(
+            "got while removing combined repo: {}",
+            Report::from_error(e)
+        );
+    }
+
+    info!("cloning local repository");
+    debug!("local repository at `{url}`");
+    let repo = {
+        let git_progress = Git::new();
+        let mut fetch_options = FetchOptions::new();
+        fetch_options.remote_callbacks(git_progress.remote_callbacks());
+        let x = RepoBuilder::new()
+            .fetch_options(fetch_options)
+            .clone(url.as_str(), &repo_path)
+            .context(GitSnafu {
+                what: "clone local repo",
+            })?;
+        x
+    };
+    if !repo.submodules().unwrap().is_empty() {
+        panic!("submodules not supported yet");
+    }
+    let repo_use = RepositoryUse::identify(&repo)?;
+
+    for (other_kind, other_repo) in repo_use.other_repos().iter().progress_ext("Merge Repos") {
+        info!("fetching {other_kind} repository");
+        debug!("{other_kind} repository at `{other_repo}`");
+        let mut remote = repo.remote_anonymous(&other_repo).context(GitSnafu {
+            what: "creating remote",
+        })?;
+        {
+            let git_progress = Git::new();
+            let mut fetch_options = FetchOptions::new();
+            fetch_options.remote_callbacks(git_progress.remote_callbacks());
+            remote
+                .fetch(&["master:master-other"], Some(&mut fetch_options), None)
+                .context(GitSnafu {
+                    what: "fetching remote repo",
+                })?;
+        }
+        let master = branch_to_commit(&repo, "master")?;
+        let master_tree = master.tree().context(GitSnafu {
+            what: "getting master tree",
+        })?;
+        let master_other = branch_to_commit(&repo, "master-other")?;
+        let other_tree = master_other.tree().context(GitSnafu {
+            what: "getting other tree",
+        })?;
+
+        let mut tree_builder = TreeUpdateBuilder::new();
+        let prefix = format!("{}/", super::CONTENT_DIR);
+        let mut walk_error: Option<Error> = None;
+        let walk_result = other_tree.walk(git2::TreeWalkMode::PreOrder, |a, b| {
+            if !a.starts_with(&prefix) {
+                if !a.is_empty() || b.name() != Some(super::CONTENT_DIR) {
+                    return TreeWalkResult::Skip;
+                }
+            }
+
+            let name = match b.name() {
+                Some(n) => n,
+                None => {
+                    walk_error = Some(
+                        UpdateTreeSnafu {
+                            msg: format!("tree entry without name in `{a}`"),
+                        }
+                        .build(),
+                    );
+                    return TreeWalkResult::Abort;
+                }
+            };
+
+            let path = format!("{}{}", a, name);
+            match b.kind() {
+                Some(ObjectType::Blob) => (),
+                Some(ObjectType::Tree) => return TreeWalkResult::Ok,
+                kind => {
+                    walk_error = Some(
+                        UpdateTreeSnafu {
+                            msg: format!("unknown blob type `{kind:?}` for `{path}`"),
+                        }
+                        .build(),
+                    );
+                    return TreeWalkResult::Abort;
+                }
+            }
+
+            if let Err(e) = check_conflict(&master_tree, Path::new(&path)) {
+                walk_error = Some(e);
+                return TreeWalkResult::Abort;
+            }
+
+            debug!("upsert `{path}`");
+            tree_builder.upsert(path, b.id(), FileMode::Blob);
+            TreeWalkResult::Ok
+        });
+
+        if let Some(error) = walk_error {
+            return Err(error);
+        }
+
+        walk_result.context(GitSnafu {
+            what: "traverse tree",
+        })?;
+
+        let merged_tree_oid = tree_builder
+            .create_updated(&repo, &master_tree)
+            .context(GitSnafu { what: "build tree" })?;
+        let merged_tree = repo.find_tree(merged_tree_oid).unwrap();
+
+        let sig = repo.signature().context(GitSnafu {
+            what: "commit signature",
+        })?;
+        let msg = format!("Merge {other_repo}");
+        repo.commit(
+            Some("HEAD"),
+            &sig,
+            &sig,
+            &msg,
+            &merged_tree,
+            &[&master, &master_other],
+        )
+        .context(GitSnafu { what: "committing" })?;
+
+        repo.checkout_head(Some(CheckoutBuilder::default().force()))
+            .context(GitSnafu {
+                what: "checkout merged",
+            })?;
+
+        repo.find_branch("master-other", BranchType::Local)
+            .context(GitSnafu {
+                what: "find master-other",
+            })?
+            .delete()
+            .context(GitSnafu {
+                what: "delete master-other",
+            })?;
+    }
+
+    Ok(())
+}
---
+++ src/progress.rs
@@ -0,0 +1,163 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use std::iter::FusedIterator;
+
+use indicatif::{
+    MultiProgress, ProgressBar, ProgressBarIter, ProgressFinish, ProgressIterator, ProgressStyle,
+};
+use indicatif_log_bridge::LogWrapper;
+use log::Log;
+
+lazy_static::lazy_static! {
+    static ref PROGRESS: MultiProgress = MultiProgress::new();
+    static ref STYLE: ProgressStyle = {
+        ProgressStyle::with_template("{prefix:>12.cyan.bold} [{bar:57}] {pos}/{len}")
+            .unwrap()
+            .progress_chars("=> ")
+    };
+}
+
+pub fn init<L: 'static + Log>(logger: L) {
+    LogWrapper::new(PROGRESS.clone(), logger)
+        .try_init()
+        .unwrap();
+}
+
+pub fn clear() {
+    PROGRESS.clear().ok();
+}
+
+pub trait ProgressIteratorExt: ProgressIterator + ExactSizeIterator {
+    fn progress_ext(self, prefix: &'static str) -> ProgressBarExtIter<Self>;
+}
+
+impl<T> ProgressIteratorExt for T
+where
+    T: ProgressIterator + ExactSizeIterator,
+{
+    fn progress_ext(self, prefix: &'static str) -> ProgressBarExtIter<Self> {
+        let len = self.len();
+        if len == 0 {
+            return ProgressBarExtIter::Without;
+        }
+
+        let bar = ProgressBar::new(len as _);
+        PROGRESS.add(bar.clone());
+        let it = self
+            .progress_with(bar.clone())
+            .with_style(STYLE.clone())
+            .with_prefix(prefix)
+            .with_finish(ProgressFinish::AndLeave);
+
+        ProgressBarExtIter::With { bar: Some(bar), it }
+    }
+}
+
+pub enum ProgressBarExtIter<T> {
+    With {
+        bar: Option<ProgressBar>,
+        it: ProgressBarIter<T>,
+    },
+    Without,
+}
+
+impl<S, T: Iterator<Item = S>> Iterator for ProgressBarExtIter<T> {
+    type Item = S;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let (it, bar) = match self {
+            Self::Without => return None,
+            Self::With { it, bar } => (it, bar),
+        };
+
+        let item = it.next();
+
+        if item.is_none() {
+            if let Some(bar) = bar.take() {
+                PROGRESS.remove(&bar);
+            }
+        }
+
+        item
+    }
+}
+
+impl<T: ExactSizeIterator> ExactSizeIterator for ProgressBarExtIter<T> {
+    fn len(&self) -> usize {
+        match self {
+            Self::With { it, .. } => it.len(),
+            Self::Without => 0,
+        }
+    }
+}
+
+impl<T: DoubleEndedIterator> DoubleEndedIterator for ProgressBarExtIter<T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        match self {
+            Self::With { it, .. } => it.next_back(),
+            Self::Without => None,
+        }
+    }
+}
+
+impl<T: FusedIterator> FusedIterator for ProgressBarExtIter<T> {}
+
+impl<T> Drop for ProgressBarExtIter<T> {
+    fn drop(&mut self) {
+        let bar = match self {
+            Self::With { bar, .. } => bar,
+            Self::Without => return,
+        };
+        if let Some(bar) = bar.take() {
+            PROGRESS.remove(&bar);
+        }
+    }
+}
+
+pub struct Git(Option<ProgressBar>);
+
+impl Git {
+    pub fn new() -> Self {
+        Self(Some(
+            PROGRESS
+                .add(ProgressBar::no_length())
+                .with_style(STYLE.clone()),
+        ))
+    }
+
+    pub fn remote_callbacks<'a>(&'a self) -> git2::RemoteCallbacks<'a> {
+        let mut cb = git2::RemoteCallbacks::new();
+
+        cb.transfer_progress(move |stats| {
+            let local = match &self.0 {
+                Some(b) => b,
+                None => return true,
+            };
+
+            if stats.received_objects() == stats.total_objects() {
+                local.set_prefix("Deltas");
+                local.set_length(stats.total_deltas() as _);
+                local.set_position(stats.indexed_deltas() as _);
+            } else if stats.total_objects() > 0 {
+                local.set_prefix("Objects");
+                local.set_length(stats.total_objects() as _);
+                local.set_position(stats.received_objects() as _);
+            }
+            true
+        });
+
+        cb
+    }
+}
+
+impl Drop for Git {
+    fn drop(&mut self) {
+        if let Some(bar) = self.0.take() {
+            PROGRESS.remove(&bar);
+        }
+    }
+}
